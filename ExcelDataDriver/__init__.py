bddfb50f1984c67db8b8f0757dbcbcd90c5da395
atthaboons <atthaboon.s@gmail.com>
2021-01-01 17:09:14 +0700

    update to v1.2.3

--- ExcelDataDriver/__init__.py
+++ ExcelDataDriver/__init__.py
@@ -40,7 +40,7 @@ from ExcelDataDriver.Keywords.CoreExcelKeywords import CoreExcelKeywords
 from ExcelDataDriver.Config.CaptureScreenShotOption import CaptureScreenShotOption
 
 
-__version__ = '1.2.2'
+__version__ = '1.2.3'
 
 
 class ExcelDataDriver:
